2fe2ab1ccc29959319345ffdfe6992706688ce8d
i-b8o <blackview1617@gmail.com>
2024-04-13 23:19:09 +0300

    added

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,16 +1,13 @@
 CREATE TABLE public.filters (
-    id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    kw_id BIGINT,
-    filter_name TEXT,
-    name TEXT,
-    filter_id BIGINT,
-    count BIGINT,
-    PRIMARY KEY (id)
+    id SERIAL PRIMARY KEY,
+    filter_name TEXT NOT NULL,
+    name TEXT NOT NULL,
+    UNIQUE (filter_name, name)
 );
 
-CREATE INDEX idx_filter_name_name ON filters (filter_name, name);
+CREATE INDEX idx_filters_on_filter_name ON filters (filter_name);
 
-CREATE TABLE search_phrases (
+CREATE TABLE public.search_phrases (
     id SERIAL PRIMARY KEY,
     phrase TEXT NOT NULL UNIQUE,
     frequency INTEGER NOT NULL DEFAULT 0
@@ -18,10 +15,10 @@ CREATE TABLE search_phrases (
 
 CREATE INDEX idx_search_phrases_phrase ON search_phrases(phrase);
 
-CREATE USER test_user WITH ENCRYPTED PASSWORD 'test_user_031501';s
-GRANT CONNECT ON DATABASE test_db TO test_user;
-GRANT ALL PRIVILEGES ON TABLE public.filters TO test_user;
-GRANT ALL PRIVILEGES ON TABLE public.search_phrases TO test_user;
-GRANT ALL ON SEQUENCE public.filters_id_seq TO test_user;
-GRANT ALL ON SEQUENCE public.search_phrases_id_seq TO test_user;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO test_user;
+CREATE USER test_user WITH ENCRYPTED PASSWORD 'filter_user_031501';s
+GRANT CONNECT ON DATABASE parsing TO filter_user;
+GRANT ALL PRIVILEGES ON TABLE public.filters TO filter_user;
+GRANT ALL PRIVILEGES ON TABLE public.search_phrases TO filter_user;
+GRANT ALL ON SEQUENCE public.filters_id_seq TO filter_user;
+GRANT ALL ON SEQUENCE public.search_phrases_id_seq TO filter_user;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO filter_user;
