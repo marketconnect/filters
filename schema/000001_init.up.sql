d4f8ef217cba6777431dfad4045698c9748aaf8d
i-b8o <blackview1617@gmail.com>
2024-04-19 21:27:17 +0300

    added

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -26,13 +26,14 @@ CREATE INDEX idx_kw_on_normquery ON kw (normquery);
 CREATE TABLE categories (
     id SERIAL PRIMARY KEY,
     kw_id INTEGER NOT NULL,
+    filter_name TEXT NOT NULL,
+    name TEXT NOT NULL,
     filter_id INTEGER NOT NULL,
     count INTEGER NOT NULL,    
     FOREIGN KEY (kw_id) REFERENCES kw (id)   
 );
 CREATE INDEX idx_categories_on_kw_id ON categories (kw_id);
 CREATE INDEX idx_categories_on_filter_id ON categories (filter_id);
-
 
 CREATE TABLE lemmas (
     id SERIAL PRIMARY KEY,
@@ -47,18 +48,18 @@ CREATE TABLE kw_lemmas (
     PRIMARY KEY (kw_id, lemma_id)
 );
 
+CREATE USER filter_user WITH ENCRYPTED PASSWORD 'filter_user_031501';
+GRANT CONNECT ON DATABASE parsing TO filter_user;
+GRANT ALL PRIVILEGES ON TABLE public.filters TO filter_user;
+GRANT ALL PRIVILEGES ON TABLE public.kw TO filter_user;
+GRANT ALL PRIVILEGES ON TABLE public.categories TO filter_user;
+GRANT ALL PRIVILEGES ON TABLE public.search_phrases TO filter_user;
+GRANT ALL PRIVILEGES ON TABLE public.kw_lemmas TO filter_user;
+GRANT ALL PRIVILEGES ON TABLE public.lemmas TO filter_user;
+GRANT ALL ON SEQUENCE public.filters_id_seq TO filter_user;
+GRANT ALL ON SEQUENCE public.kw_id_seq TO filter_user;
+GRANT ALL ON SEQUENCE public.categories_id_seq TO filter_user;
+GRANT ALL ON SEQUENCE public.search_phrases_id_seq TO filter_user;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO filter_user;
 
-CREATE USER test_user WITH ENCRYPTED PASSWORD 'test_user_031501';
-GRANT CONNECT ON DATABASE parsing TO test_user;
-GRANT ALL PRIVILEGES ON TABLE public.filters TO test_user;
-GRANT ALL PRIVILEGES ON TABLE public.kw TO test_user;
-GRANT ALL PRIVILEGES ON TABLE public.categories TO test_user;
-GRANT ALL PRIVILEGES ON TABLE public.search_phrases TO test_user;
-GRANT ALL PRIVILEGES ON TABLE public.kw_lemmas TO test_user;
-GRANT ALL PRIVILEGES ON TABLE public.lemmas TO test_user;
-GRANT ALL ON SEQUENCE public.filters_id_seq TO test_user;
-GRANT ALL ON SEQUENCE public.kw_id_seq TO test_user;
-GRANT ALL ON SEQUENCE public.categories_id_seq TO test_user;
-GRANT ALL ON SEQUENCE public.search_phrases_id_seq TO test_user;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO test_user;
 
